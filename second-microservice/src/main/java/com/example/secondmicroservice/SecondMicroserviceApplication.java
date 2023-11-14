3cdad322d80d35e5e6a09def27dadb0a832867ed
DeevS <deev@advantum.pro>
2023-11-14 20:43:09 +0300

    Multi-project structure

--- second-microservice/src/main/java/com/example/secondmicroservice/SecondMicroserviceApplication.java
+++ second-microservice/src/main/java/com/example/secondmicroservice/SecondMicroserviceApplication.java
@@ -1,13 +1,13 @@
-package com.example.app;
+package com.example.secondmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SecondMicroserviceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(SecondMicroserviceApplication.class, args);
     }
 
 }
