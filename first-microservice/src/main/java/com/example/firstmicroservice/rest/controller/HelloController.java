adbabeed068f7aa345acd0e26d1e58979e468020
DeevS <deev@advantum.pro>
2023-11-15 20:42:30 +0300

    Add controller and service to first microservice

---
+++ first-microservice/src/main/java/com/example/firstmicroservice/rest/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.firstmicroservice.rest.controller;
+
+import com.example.firstmicroservice.service.HelloService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    HelloService helloService;
+
+    @GetMapping
+    ResponseEntity<String> hello(){
+        return ResponseEntity.ok(helloService.hello());
+    }
+
+}
