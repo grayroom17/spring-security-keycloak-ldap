14e257087ade91bc923b95160a177f7e086f05ea
DeevS <deev@advantum.pro>
2023-11-16 20:04:38 +0300

    First Microservice. Add Feign Client

--- first-microservice/src/main/java/com/example/firstmicroservice/service/HelloService.java
+++ first-microservice/src/main/java/com/example/firstmicroservice/service/HelloService.java
@@ -1,12 +1,24 @@
 package com.example.firstmicroservice.service;
 
+import com.example.firstmicroservice.client.SecondMicroserviceClient;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
 @Service
 public class HelloService {
 
+    SecondMicroserviceClient secondMicroserviceClient;
+
     public String hello() {
         return "Hello World!";
+    }
+
+    public String sayHelloToSecondMicroservice() {
+        return secondMicroserviceClient.hello();
     }
 
 }
